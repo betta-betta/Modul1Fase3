88aeaa694c9248acc4319d69b4f7a9ef0959410f
Lluis Gutierrez <betta_betta@hotmail.com>
2021-02-26 16:03:38 +0100

    Nodul1Fase3

---
+++ src/Modul1Fase3App.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+public class Modul1Fase3App {
+
+	public static void main(String[] args) {
+		
+		final int ANY=1948;
+		int contador=0;
+		boolean comprobacio=false;
+		String correcte="El teu any de naixement  és de traspàs";
+		String incorrecte="El teu any de naixement no és de traspàs";
+		
+				String naixement=JOptionPane.showInputDialog("Introdueix el teu any de naixement");
+		int anyNaixement = Integer.parseInt(naixement);
+		for(int i=ANY;i<=anyNaixement;i++){
+			if((i % 4 == 0)&&(i % 100 != 100)|| (i % 400 == 0))
+			{
+			System.out.println("any " + i);
+			contador++;	
+			if(i == anyNaixement){
+				comprobacio = true;
+				
+			}
+		}
+		}
+			System.out.println("El total d'anys de traspàs entre  " + ANY + " i " + anyNaixement + ""
+			+ " són: " + contador);
+			if (comprobacio == true){
+				JOptionPane.showMessageDialog(null,correcte);
+			}
+			else{
+				JOptionPane.showMessageDialog(null,incorrecte);
+				
+			}
+	
+	}
+	}
